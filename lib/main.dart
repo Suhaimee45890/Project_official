df26a0c5b26a1255b055bb49b34c1fa2e1371bd0
Siaberm <siabermfacebook@gmail.com>
2025-06-25 10:24:47 +0700

    setup

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "HalalPro",
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Project", style: TextStyle(color: Colors.red)),
+      ),
+      body: TextField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          hint: Text('email'),
+        ),
+      ),
+    );
+  }
+}
